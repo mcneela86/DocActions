ee272c4032aea82cb5559c3100508e813438f76c
theox26 <theox26@gmail.com>
2014-03-10 09:22:04 -0400

    Relative argument instead of hard coded

--- SequenceNumbers.js
+++ SequenceNumbers.js
@@ -5,12 +5,12 @@ import System.Diagnostics;
 
 class Actions
 {
-	static function sequenceNumb() {
+	static function sequenceNumb(phrase) { //phrase is a string of text to be replaced, make it unique so it doesn't grab unwanted text
 		//get the current record and place it on each page	
 		var currentRecord = Application.CurrentRecordNumber();
 		currentRecord = ' 00' + currentRecord.toString();
 		//Application.Log("Current Record is:"+ currentRecord);
-		var numb = Application.CurrentDocument.FindText('Sequence Number Thing',true,true);
+		var numb = Application.CurrentDocument.FindText(phrase,true,true);
 		for(var i = 0; i < numb.length; i++){
 		numb[i].Text = currentRecord;
 		}
