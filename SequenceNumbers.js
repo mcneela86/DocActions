7c75e2359f79bedb24968fe16033f23be9abba20
theox26 <theox26@gmail.com>
2014-03-10 10:22:18 -0400

    fixed path

--- SequenceNumbers.js
+++ SequenceNumbers.js
@@ -5,12 +5,13 @@ import System.Diagnostics;
 
 class Actions
 {
-	static function sequenceNumb() {
+
+	static function sequenceNumb(path) {
 		//get the current record and place it on each page	
 		var currentRecord = Application.CurrentRecordNumber();
 		currentRecord = ' 00' + currentRecord.toString();
 		//Application.Log("Current Record is:"+ currentRecord);
-		var numb = Application.CurrentDocument.FindText('Sequence Number Thing',true,true);
+		var numb = Application.CurrentDocument.FindText(path,true,true);
 		for(var i = 0; i < numb.length; i++){
 		numb[i].Text = currentRecord;
 		}
