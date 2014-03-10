20bc6d1ce66f6a33ad4b3c5e72592d5e28b1aa7a
theox26 <theox26@gmail.com>
2014-03-10 10:33:58 -0400

    Never change the same file without committing

--- SequenceNumbers.js
+++ SequenceNumbers.js
@@ -5,18 +5,14 @@ import System.Diagnostics;
 
 class Actions
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 7a5382389d92235ecc1cce18e57f7fe2a2742a09
 	static function sequenceNumb(path) {
-		//get the current record and place it on each page	
+		//get the current record and place it on each page
 		var currentRecord = Application.CurrentRecordNumber();
 		currentRecord = ' 00' + currentRecord.toString();
 		//Application.Log("Current Record is:"+ currentRecord);
 		var numb = Application.CurrentDocument.FindText(path,true,true);
 		for(var i = 0; i < numb.length; i++){
-		numb[i].Text = currentRecord;
+			numb[i].Text = currentRecord;
 		}
 	}	
 }
