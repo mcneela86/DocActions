e03f16a24d29f75e7af664c87f9cd01a4412c8c6
theox26 <theox26@gmail.com>
2014-03-10 10:35:56 -0400

    path to phrase

--- SequenceNumbers.js
+++ SequenceNumbers.js
@@ -5,12 +5,12 @@ import System.Diagnostics;
 
 class Actions
 {
-	static function sequenceNumb(path) {
+	static function sequenceNumb(phrase) { //phrase to find and replace with sequence number
 		//get the current record and place it on each page
 		var currentRecord = Application.CurrentRecordNumber();
 		currentRecord = ' 00' + currentRecord.toString();
 		//Application.Log("Current Record is:"+ currentRecord);
-		var numb = Application.CurrentDocument.FindText(path,true,true);
+		var numb = Application.CurrentDocument.FindText(phrase,true,true);
 		for(var i = 0; i < numb.length; i++){
 			numb[i].Text = currentRecord;
 		}
